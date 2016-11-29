581f86222a3ec886c721e59338944d20b5b9ed22
Danny Byus <dakunesu@gmail.com>
2016-11-28 21:16:49 -0700

    Updated flutter, started Dnd building

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
-String title = 'Flutter Learning stuff';
-
-//entry point
+//Entry point
 void main() {
-  runApp(new MaterialApp(
-      title: title,
-      theme: new ThemeData(primarySwatch: Colors.red),
-      home: new FlutterDemo()));
+  runApp(
+    new MaterialApp(
+      title: 'Dnd',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new FlutterDemo(),
+    ),
+  );
 }
 
-//base widget for the whole app
-//I guess this is needed
+//base stateful widget
 class FlutterDemo extends StatefulWidget {
   FlutterDemo({Key key}) : super(key: key);
 
@@ -19,68 +21,43 @@ class FlutterDemo extends StatefulWidget {
   _FlutterDemoState createState() => new _FlutterDemoState();
 }
 
-//Demo app itself
+//base stateful widget's state
 class _FlutterDemoState extends State<FlutterDemo> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  //
+  Map<String, Input> _inputs = {};
+  Map<String, InputValue> _values = {
+    'Name': const InputValue(),
+    'Race': const InputValue(),
+    'Class': const InputValue()
+  };
 
-  int _counter = 0;
-
-  //builder functions used to expose context for Scaffold.of(context) options.
-  //without this structure, you cannot do things like snackbars
-
-  //builder function for body
-  Widget buildBody(BuildContext context) {
-    return new Center(child: new Text('banana tapped$_counter'));
-  }
-
-  //builder function for floating action
-  Widget buildFab(BuildContext context) {
-    return new FloatingActionButton(onPressed: () {
-      //set state rebuilds a stated whidget
-      setState(() {
-        _counter++;
-
-        //toast options
-        /*Scaffold
-            .of(context)
-            .showSnackBar(new SnackBar(content: new Text('Opened')));*/
-      });
-
-      _scaffoldKey.currentState
-          .showBottomSheet /*<Null>*/ ((BuildContext context) {
-        final ThemeData themeData = Theme.of(context);
-        return new Container(
-            decoration: new BoxDecoration(
-                border: new Border(
-                    top: new BorderSide(color: themeData.disabledColor))),
-            child: new Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: new Block(children: <Widget>[
-                  new Text('Short bottom message',
-                      textAlign: TextAlign.center,
-                      style: new TextStyle(
-                          color: themeData.accentColor, fontSize: 24.0))
-                ])));
-      }).closed.then((_) {
-        setState(() {
-          // re-enable the button
-          //_showBottomSheetCallback = showBottomSheet;
-          Scaffold
-              .of(context)
-              .showSnackBar(new SnackBar(content: new Text('Closed')));
-        });
-      });
-    }, tooltip: 'Increment', child: new Icon(Icons.add));
-  }
-
-  //main build point
-  //scaffold is a pre-bake
   @override
   Widget build(BuildContext context) {
+    _values.keys.forEach((e) {
+      _inputs[e] = new Input(
+          onChanged: (InputValue _input) {
+            setState(() {
+              _values[e] = _input;
+            });
+          },
+          value: _values[e],
+          labelText: e);
+    });
+
+    List boxes = _inputs.keys.toList().map((e) {
+      return _inputs[e];
+    }).toList();
+
     return new Scaffold(
-        key: _scaffoldKey,
-        appBar: new AppBar(title: new Text(title)),
-        body: new Builder(builder: buildBody),
-        floatingActionButton: new Builder(builder: buildFab));
+      appBar: new AppBar(
+        title: new Text('Dnd'),
+      ),
+      body: new Block(children: boxes),
+      floatingActionButton: new FloatingActionButton(
+        //onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),
+    );
   }
 }
