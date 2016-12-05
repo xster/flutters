a24f80181db757b927e9a08543c0e2440c61f932
Danny Byus <dakunesu@gmail.com>
2016-12-05 07:23:02 -0700

    More progress on flutter based rabbit numbers

--- rabbit_numbers/lib/main.dart
+++ rabbit_numbers/lib/main.dart
@@ -4,22 +4,33 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
+String title = 'Rabbit numbers';
+GlobalKey<ScaffoldState> _scaffoldKey;
+
+  //make toasts
+  void showInSnackBar(String value) {
+    _scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text(value)));
+  }
+
 //Entry point
 void main() {
   runApp(
     new MaterialApp(
-      title: 'Rabbit numbers',
+      title: title,
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new FlutterDemo(),
+      home: new FlutterDemo(title: title),
     ),
   );
 }
 
 //base stateful widget
 class FlutterDemo extends StatefulWidget {
-  FlutterDemo({Key key}) : super(key: key);
+  FlutterDemo({this.title, Key key}) : super(key: key);
+
+  final String title;
 
   @override
   _FlutterDemoState createState() => new _FlutterDemoState();
@@ -27,25 +38,20 @@ class FlutterDemo extends StatefulWidget {
 
 //base stateful widget's state
 class _FlutterDemoState extends State<FlutterDemo> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
     super.initState();
     //do stuff when state is initialized
+    _scaffoldKey = new GlobalKey<ScaffoldState>();
   }
 
   //html localhost
+  //get data stored to get and save data
   Future<File> _get_data() async {
     // get the path to the document directory.
     String dir = (await PathProvider.getApplicationDocumentsDirectory()).path;
     return new File('$dir/data.txt');
-  }
-
-  //make toasts
-  void showInSnackBar(String value) {
-    _scaffoldKey.currentState
-        .showSnackBar(new SnackBar(content: new Text(value)));
   }
 
   int move_cost = 20;
@@ -56,7 +62,7 @@ class _FlutterDemoState extends State<FlutterDemo> {
     return new Scaffold(
         key: _scaffoldKey,
         appBar: new AppBar(
-            title: new Text('Rabbit Numbers'),
+            title: new Text(config.title),
             actions: <Widget>[
               new IconButton(
                   onPressed: () {
@@ -66,30 +72,63 @@ class _FlutterDemoState extends State<FlutterDemo> {
                   },
                   tooltip: 'Double up for $move_cost',
                   icon: new Icon(Icons.add)),
-              new PopupMenuButton<String>(
-                  onSelected: showInSnackBar,
-                  itemBuilder: (BuildContext context) =>
-                      <PopupMenuItem<String>>[
-                        new PopupMenuItem<String>(
-                            value: 'Toolbar menu',
-                            child: new Text('Toolbar menu')),
-                        new PopupMenuItem<String>(
-                            value: 'Right here', child: new Text('Right here')),
-                        new PopupMenuItem<String>(
-                            value: 'Hooray!', child: new Text('Hooray!')),
-                      ])
+              new IconButton(
+                  onPressed: () {
+                    setState(() {
+                      move_cost *= 2;
+                    });
+                  },
+                  tooltip: 'Shuffle for $move_cost',
+                  icon: new Icon(Icons.shuffle)),
+              new IconButton(
+                  onPressed: () {
+                    setState(() {
+                      move_cost *= 2;
+                    });
+                  },
+                  tooltip: 'Restart',
+                  icon: new Icon(Icons.refresh))
             ],
             leading: new Image.asset('assets/nuRabbit.png')),
         body: new Padding(
             padding: const EdgeInsets.all(16.0),
-            child: new Block(children: [
-              new LinearProgressIndicator(),
-              new Image.asset('assets/nuRabbit.png')
+            child: new Column(children: [
+              new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [new Tile(value: 1)])
             ])),
         floatingActionButton: new FloatingActionButton(
           onPressed: () {},
           tooltip: 'Increment',
           child: new Icon(Icons.add),
         ));
+  }
+}
+
+//yay new widget
+class Tile extends StatefulWidget {
+  Tile({this.value: 0, Key key}) : super(key: key);
+
+  final int value;
+
+  @override
+  Tile_state createState() => new Tile_state();
+}
+
+class Tile_state extends State<Tile> {
+
+  @override
+  void initState() {
+    super.initState();
+    //do stuff when state is initialized
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+        child: new InkWell(onTap:()=>showInSnackBar('hi'),
+            child: new Padding(
+                padding: const EdgeInsets.all(116.0),
+                child: new Text('${config.value}'))));
   }
 }
